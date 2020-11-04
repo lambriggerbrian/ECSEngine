77d18e551e835cb85f58577d3c5c43aa383f9367
lambriggerbrian <lambrigger.brian@gmail.com>
2020-11-03 16:29:16 -0800

    Add project files.

---
+++ ISystem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Engine
+{
+    public interface ISystem
+    {
+        public void Simulate(Single timestep);
+
+        public Boolean RegisterComponent(Entity entity);
+
+        public Component GetComponent(Entity entity);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Engine
+{
+    internal class Program
+    {
+        private static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
